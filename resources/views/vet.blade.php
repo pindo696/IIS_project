c832dce3e3ad82057dc11e1a69b5ede3fef9ac34
Light-ghm <majo19991@gmail.com>
2022-11-20 06:04:09 +0100

    vet progress

--- resources/views/vet.blade.php
+++ resources/views/vet.blade.php
@@ -1,113 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    
-                {{--<div class="card">
-                    <div class="card-header">{{ __('Vet Page') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-    
-            // UNDER CONSTRUCTION
-        veterinář:
-        vyřizuje požadavky od pečovatele (plánuje vyšetření zvířat dle požadavků)
-        udržuje zdravotní záznamy zvířat
-
-            
-            --}}
-
-
 
 <head>
     <link href="{{asset('css/fontawesome.css')}}" rel="stylesheet">
     <link href="{{asset('css/all.css')}}" rel="stylesheet">
 </head>
-<div style="width:97%; margin:auto;"class="row justify-content-center">
+<div style="width:97%; margin:auto; text-align: center;"class="row justify-content-center">
     <div class="col-md-7">
         <div class="card">
             <div class="card-header text-center">{{ __('Animal examination requests') }}</div>
                 <div class="card-body">
                     <table class="table text-center">
                         <thead class="bg-white">
-                            <tr>
+                            <tr style="vertical-align: middle;">
                             <th>Applicant</th>
-                            <th>Animal ID</th>
                             <th>Animal Name</th>
                             <th>Species</th>
                             <th>Description</th>
+                            <th>Status</th>
                             <th>Administer</th>
                             </tr>
                         </thead>
                         <tbody>
-                {{--
-                        @foreach($result as $data)
-                            @php ($style = '#FFFFFF')
-                            @if($data->confirmation == 'waiting')
-                                @php ($style = 'text-orange')
-                            @endif
-                            @if($data->confirmation == 'accepted')
-                                @php ($style = 'text-success')
-                            @endif
-                            @if($data->confirmation == 'declined')
-                                @php ($style = 'text-danger')
-                            @endif
-                            <tr class="alert text-center" role="alert">
-                            <td>{{$data->name}}</td>
-                            <td>{{$data->surname}}</td>
-                            <td>{{$data->email}}</td>
-                            @if($data->confirmation == 'banned')
-                                <td class="{{$style}}"><b>{{$data->confirmation}}</b></td>
-                            @else
-                                <td class="{{$style}}">{{$data->confirmation}}</td>
-                            @endif
-                            <td style="text-align: left;">
-                            @if($data->confirmation == 'accepted')
-                                <form action="/careman/banVolunteer/" method="POST">
-                                @csrf
-                                <input type="hidden" id="data_id" name="data_id" value="{{$data->id}}">
-                                <button title="Ban volunteer" class="btn text-danger fa-solid fa-ban"></button>
-                                </form>
-                                                    @elseif($data->confirmation == 'declined')
-                                                        <form action="/careman/acceptVolunteer/" method="POST">
-                                                            @csrf
-                                                            @method('POST')
-                                                            <input type="hidden" id="data_id" name="data_id" value="{{$data->id}}">
-                                                            <button title="Accept volunteer" type="submit" class="btn text-success fa-solid fa-check"></button>
-                                                        </form>
-                                                    @elseif($data->confirmation == 'banned')
-                                                        <form action="/careman/deleteVolunteer/" method="POST">
-                                                            @csrf
-                                                            @method('POST')
-                                                            <input type="hidden" id="data_id" name="data_id" value="{{$data->id}}">
-                                                            <button title="Delete user" type="submit" class="btn text-danger fa-solid fa-trash-can"></button>
-                                                        </form>
-                                                    @else
-                                                        <div class="d-inline-block">
-                                                        <form class="d-inline" action="/careman/acceptVolunteer/" method="POST">
-                                                            @csrf
-                                                            @method('POST')
-                                                            <input type="hidden" id="data_id" name="data_id" value="{{$data->id}}">
-                                                            <button title="Accept volunteer" type="submit" class="btn text-success fa-solid fa-check"></button>
-                                                        </form>
-                                                        <form class="d-inline" action="/careman/declineVolunteer/" method="POST">
-                                                            @csrf
-                                                            @method('POST')
-                                                            <input type="hidden" id="data_id" name="data_id" value="{{$data->id}}">
-                                                            <button title="Decline volunteer" type="submit" class="btn text-danger fa-solid fa-xmark"></button>
-                                                        </form>
-                                                        </div>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @endforeach --}}
+            
+                            @foreach ($examinations as $examination)
+                                @php ($style = '#FFFFFF')
+                                @if($examination->examination_status == 'requested')
+                                    @php ($style = 'text-orange')
+                                @endif
+                                @if($examination->examination_status == 'done')
+                                    @php ($style = 'text-success')
+                                @endif
+                                @if($examination->examination_status == 'planned')
+                                    @php ($style = 'text-info')
+                                @endif
+                                <tr style="vertical-align: middle;" class="alert text-center" role="alert">
+                                <td>{{$examination->name . " " . $examination->surname}}</td>
+                                <td>{{$examination->animal_name}}</td>
+                                <td>{{$examination->species}}</td>
+                                <td>{{$examination->examination_description}}</td>
+                                <td class="{{$style}}">{{$examination->examination_status}}</td>
+                                <td>  
+                                    <form action="/vet/request/{{$examination->examination_id}}" method="POST">
+                                        @csrf
+                                        <input type="hidden" id="examination_id" name="examination_id" value="{{$examination->examination_id}}">
+                                        <button title="Administer" class="btn text-primary fa-solid fa-book"></button>
+                                    </form>
+                                </td>
+                                </tr>
+                            @endforeach
+
                         </tbody>
                     </table>
                 </div>
@@ -119,7 +62,7 @@
                 <div class="card-body">
                     <table class="table text-center">
                         <thead class="bg-white">
-                            <tr>
+                            <tr style="vertical-align: middle;">
                             <th>Animal ID</th>
                             <th>Animal Name</th>
                             <th>Species</th>
@@ -128,7 +71,23 @@
                             </tr>
                         </thead>
                         <tbody>
-                            
+
+                            @foreach ($records as $record)
+                                <tr style="vertical-align: middle;" class="alert text-center" role="alert">
+                                <td>{{$record->animal_id}}</td>
+                                <td>{{$record->animal_name}}</td>
+                                <td>{{$record->species}}</td>
+                                <td>{{$record->animal_age}}</td>
+                                <td>  
+                                    <form action="/vet/records/animal/{{$record->animal_id}}" method="POST">
+                                        @csrf
+                                        <input type="hidden" id="animal_id" name="animal_id" value="{{$record->animal_id}}">
+                                        <button title="Show records" class="btn text-primary fa-solid fa-eye"></button>
+                                    </form>
+                                </td>
+                                </tr>
+                            @endforeach
+
                         </tbody>
                     </table>
                 </div>
