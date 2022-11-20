3d0884c004fd1bae5eef160b97cb1b8ef6194beb
Light-ghm <majo19991@gmail.com>
2022-11-20 14:08:36 +0100

    vetfix

--- app/Models/Medical_Records.php
+++ app/Models/Medical_Records.php
@@ -13,11 +13,5 @@ class Medical_Records extends Model
     protected $fillable = ['medical_record_id', 'fk_animal_id', 'fk_user_id', 'record_type', 'medical_description', 'medical_date'];
 
 
-    public function db_getAnimalRecordDetailed($id){
-        $records = DB::select("SELECT * FROM medical_records JOIN animals ON medical_records.fk_animal_id=animals.animal_id JOIN users ON medical_records.fk_user_id=users.id WHERE fk_animal_id=$id");
-        return [
-            'records' => $records
-        ];
-    }
 
 }
